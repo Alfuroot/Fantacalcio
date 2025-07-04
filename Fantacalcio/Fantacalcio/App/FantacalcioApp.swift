8a465ff8096ad40fd165c4cdfac3e5d90c5208c0
Alfuroot <59538409+Alfuroot@users.noreply.github.com>
2025-07-04 19:37:30 +0200

    feat: project Creation and setup

---
+++ Fantacalcio/Fantacalcio/App/FantacalcioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantacalcioApp.swift
+//  Fantacalcio
+//
+//  Created by Giuseppe Carannante on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FantacalcioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fantacalcio/FantacalcioTests/FantacalcioTests.swift
@@ -0,0 +1,17 @@
+//
+//  FantacalcioTests.swift
+//  FantacalcioTests
+//
+//  Created by Giuseppe Carannante on 02/07/2025.
+//
+
+import Testing
+@testable import Fantacalcio
+
+struct FantacalcioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
